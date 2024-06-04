cede7fa7436ae5e3b79588052f9b75b9c6921c81
adostacey <aaron.stacey@proton.me>
2024-06-04 14:24:14 -0500

    Add menus app

---
+++ menus/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=75)
+    address = models.CharField(max_length=75)
+    description = models.TextField(blank=True)
+    hours_of_operation = models.CharField(max_length=100)
+    facebook = models.URLField(blank=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Category(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+
+    def __str__(self) -> str:
+        return f"{self.name} ({self.restaurant.name[:4]})"
+
+
+class Item(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    Category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=75)
+    description = models.TextField(blank=True)
+    price_1 = models.FloatField()
+    price_1_descr = models.CharField(max_length=50, blank=True)
+    price_2 = models.FloatField(null=True, blank=True)
+    price_2_descr = models.CharField(max_length=50, blank=True)
+
+    def __str__(self) -> str:
+        return self.name
