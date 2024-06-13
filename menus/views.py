9d237d4b581eb9e9c0fa91b19fef9b1ce8420ba0
adostacey <aaron.stacey@proton.me>
2024-06-13 09:40:42 -0500

    Update views

--- menus/views.py
+++ menus/views.py
@@ -1,3 +1,16 @@
+from typing import Any
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Category, Item, Restaurant
 
-# Create your views here.
+
+class MenuView(ListView):
+    model = Category
+    template_name = "menu.html"
+    context_object_name = "categories"
+
+    def get_context_data(self, **kwargs) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["restaurant"] = "John's Waffle & Pancake House"
+        return context
+    
--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
+from django.views.generic import ListView
+from menus.models import Restaurant
 # Create your views here.
+
+class HomeView(ListView):
+    model = Restaurant
+    template_name = "home.html"
+    context_object_name = "restaurants"
