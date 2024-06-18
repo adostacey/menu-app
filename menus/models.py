727568c6089c316282a9d98077c005954972b4c9
adostacey <aaron.stacey@proton.me>
2024-06-17 20:52:16 -0500

    Update models

--- menus/models.py
+++ menus/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Restaurant(models.Model):
@@ -7,9 +8,14 @@ class Restaurant(models.Model):
     description = models.TextField(blank=True)
     hours_of_operation = models.CharField(max_length=200)
     facebook = models.URLField(blank=True)
+    phone = models.CharField(max_length=12, null=True)
+    slug = models.SlugField()
 
     def __str__(self) -> str:
         return self.name
+
+    def get_absolute_url(self):
+        return reverse("menu", kwargs={"slug": self.slug})
 
 
 class Category(models.Model):
@@ -19,6 +25,7 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = "Categories"
+        ordering = ["name"]
 
     def __str__(self) -> str:
         return f"{self.name} ({self.restaurant.name[:4]})"
@@ -33,6 +40,9 @@ class Item(models.Model):
     price_1_descr = models.CharField(max_length=50, blank=True)
     price_2 = models.FloatField(null=True, blank=True)
     price_2_descr = models.CharField(max_length=50, blank=True)
+
+    class Meta:
+        ordering = ["name"]
 
     def __str__(self) -> str:
         return self.name
