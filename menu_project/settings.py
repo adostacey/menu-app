a5d4176a4295fdf9af834c6c1d7eb620ec0ff758
adostacey <aaron.stacey@proton.me>
2024-07-23 16:32:12 -0500

    secure_proxy_ssl_header and allowed_hosts update

--- menu_project/settings.py
+++ menu_project/settings.py
@@ -113,6 +113,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
