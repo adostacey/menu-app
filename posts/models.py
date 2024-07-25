a385144f8fafc0528bb40ae6036612373d9de832
adostacey <aaron.stacey@proton.me>
2024-07-25 11:06:43 -0500

    Init Post model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    body = models.TextField(max_length=200)
+
+    def __str__(self) -> str:
+        return self.body[:25]
