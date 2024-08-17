127802e944ed4dd7322fcf99a2d40b643b717654
adostacey <aaron.stacey@proton.me>
2024-08-17 17:31:19 -0500

    Init

--- menu_project/urls.py
+++ menu_project/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("", include("pages.urls")),
+    path("dashboard/", include("dashboard.urls")),
     path("menu/", include("menus.urls")),
     path("admin/", admin.site.urls),
 ]
